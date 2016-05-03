7d339b28921c0489e2fe2c3550c5d07463329739
Muhammad Saeed <business@evh98.com>
2016-05-03 20:41:26 +0100

    Updates switched to zip format

--- vision/src/com/evh98/vision/util/Update.java
+++ vision/src/com/evh98/vision/util/Update.java
@@ -10,9 +10,12 @@ import org.apache.commons.io.FileUtils;
 
 import com.evh98.vision.Vision;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
 public class Update {
 
-	/*
+	/**
 	 * Checks if there is an update available for Vision
 	 */
 	public static boolean isAvailable() {
@@ -32,13 +35,22 @@ public class Update {
 		return false;
 	}
 	
-	/*
+	/**
 	 * Downloads the latest Vision.jar from the server
 	 */
 	public static void downloadUpdate() {
+		// Download zip
 		try {
-			FileUtils.copyURLToFile(new URL("http://www.evh98.com/vision/vision.jar"), new File(java.lang.System.getProperty("user.home") + "/Vision/vision-latest.jar"));
+			FileUtils.copyURLToFile(new URL("http://www.evh98.com/vision/vision.zip"), new File(java.lang.System.getProperty("user.home") + "/Vision/vision.zip"));
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Extract zip
+		try {
+			ZipFile file = new ZipFile(java.lang.System.getProperty("user.home") + "/Vision/vision.zip");
+			file.extractAll(java.lang.System.getProperty("user.home") + "/Vision");
+		} catch (ZipException e) {
 			e.printStackTrace();
 		}
 	}
