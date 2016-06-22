f6994adfcb3e65a49685dd4e057ea679da76f1e8
EddieVanHalen98 <ms@evh98.com>
2016-06-23 00:14:51 +0100

    Rewrote main class for fluidity

--- vision/src/com/evh98/vision/Vision.java
+++ vision/src/com/evh98/vision/Vision.java
@@ -1,36 +1,15 @@
-/**
- * Vision - Created and owned by Muhammad Saeed (EddieVanHalen98)
- * 
- * Vision.java
- * The main class
- * 
- * File created on 18th April 2016
- */
-
 package com.evh98.vision;
 
-import java.io.IOException;
-import java.util.logging.Level;
-
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
-import com.evh98.vision.apps.NetflixScreen;
-import com.evh98.vision.apps.VideoScreen;
-import com.evh98.vision.apps.YouTubeListener;
 import com.evh98.vision.apps.YouTubeScreen;
-import com.evh98.vision.screens.AppScreen;
-import com.evh98.vision.screens.GameScreen;
 import com.evh98.vision.screens.MainScreen;
 import com.evh98.vision.screens.MediaScreen;
 import com.evh98.vision.screens.Screen;
-import com.evh98.vision.screens.SystemScreen;
-import com.evh98.vision.screens.UpdateScreen;
 import com.evh98.vision.util.Graphics;
 import com.evh98.vision.util.Palette;
-import com.evh98.vision.util.RemoteListener;
 import com.evh98.vision.util.Update;
-import com.teamdev.jxbrowser.chromium.BrowserCore;
-import com.teamdev.jxbrowser.chromium.LoggerProvider;
+
+import com.jmr.wrapper.server.Server;
+import com.teamdev.jxbrowser.chromium.Browser;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -50,119 +29,84 @@ import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
 public class Vision extends Application {
-	
+
 	public static int BUILD_NUMBER = 1;
-	public static float SCALE = Graphics.F_HD;
-	public static float WIDTH = 3840;
-	public static float HEIGHT = 2160;
-	public static boolean FULLSCREEN = true;
-	public static float VOLUME = 1.0F;
-	
-	int x = 0;
-	int y = 0;
-	
+	public static float WIDTH = 1260;
+	public static float HEIGHT = 540;
+	public static float[] ANCHOR = {WIDTH / 2, HEIGHT / 2};
+	public static float SCALE = HEIGHT / 2160;
+	public static float HORIZONTAL_SCALE = (3840.0F / 2160.0F) / (WIDTH / HEIGHT);
+	public static boolean FULLSCREEN = false;
+
 	public static Server server;
-	public static RemoteListener main_listener;
-	public static YouTubeListener youtube_listener;
-	
-	public static Group root;
+	public static Browser browser;
+
 	public static Stage main_stage;
-	public static Scene main_scene;
-	
+
 	public static Screen current_screen;
 	public static MainScreen main_screen;
-	public static GameScreen game_screen;
 	public static MediaScreen media_screen;
-	public static AppScreen app_screen;
-	public static SystemScreen system_screen;
-	public static UpdateScreen update_screen;
-	
-	public static VideoScreen video_screen;
-	public static NetflixScreen netflix_screen;
 	public static YouTubeScreen youtube_screen;
-	
+
 	public static void main(String[] args) {
+		// Init browser
+		browser = new Browser();
+
+		// Load graphics
+		Graphics.load();
+
 		launch(args);
 	}
-	
-	public void init() {
-		BrowserCore.initialize();
-		LoggerProvider.setLevel(Level.OFF);
-		
-		Graphics.load();
-	}
-	
+
+	@Override
 	public void start(Stage stage) {
+		System.out.println(HORIZONTAL_SCALE);
+
+		stage.setTitle("Vision");
+
+		// Init icons
+		setIcons(stage);
+
+		// Init server
+		server = new Server(4567, 28960);
+
 		/*
-		 * Sets up Server
+		 * Init scene
 		 */
-		server = new Server();
-		server.start();
-		try {
-			server.bind(4567, 28960);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		main_listener = new RemoteListener();
-		youtube_listener = new YouTubeListener();
-		server.addListener(main_listener);
-		
-		/*
-		 * Sets up title and icons
-		 */
+		Group root = new Group();
+		Scene scene = new Scene(root, Palette.LIGHT_GRAY);
+		scene.getStylesheets().add("fonts.css");
+		scene.setCursor(Cursor.NONE);
+
 		main_stage = stage;
-		main_stage.setTitle("Vision");
-		main_stage.getIcons().add(new Image("file:assets/icons/1024.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/512.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/256.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/128.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/64.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/48.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/32.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/24.png"));
-		main_stage.getIcons().add(new Image("file:assets/icons/16.png"));
-		
-		/*
-		 * Sets up scene
-		 */
-		root = new Group();
-		main_scene = new Scene(root, Palette.LIGHT_GRAY);
-		main_scene.getStylesheets().add("fonts.css");
-		main_scene.setCursor(Cursor.NONE);
-		main_stage.setScene(main_scene);
+		main_stage.setScene(scene);
 		main_stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
 		main_stage.setFullScreen(FULLSCREEN);
 		main_stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent t) {
-                Platform.exit();
-                System.exit(0);
-            }
+			@Override
+			public void handle(WindowEvent t) {
+				Platform.exit();
+				System.exit(0);
+			}
 		});
-		Canvas canvas = new Canvas(WIDTH * SCALE, HEIGHT * SCALE);
+		Canvas canvas = new Canvas(WIDTH, HEIGHT);
 		root.getChildren().add(canvas);
-		
-		/*
-		 * Sets up screens
-		 */		
+
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		
+		/*
+		 * Init scene
+		 */
+
+		// Init screens
 		main_screen = new MainScreen(gc);
-		game_screen = new GameScreen(gc);
 		media_screen = new MediaScreen(gc);
-		app_screen = new AppScreen(gc);
-		system_screen = new SystemScreen(gc);
-		update_screen = new UpdateScreen(gc);
-		
-		video_screen = new VideoScreen(gc);
-		netflix_screen = new NetflixScreen(gc);
 		youtube_screen = new YouTubeScreen(gc);
-		
+
 		// Checks for update
 		if (!Update.isAvailable()) {
-			setScreen(video_screen);
+			setScreen(main_screen);
 		} else {
-			setScreen(update_screen);
+//			setScreen(update_screen);
 		}
 
 		/*
@@ -175,15 +119,33 @@ public class Vision extends Application {
 				gc.clearRect(0, 0, 3840 * Vision.SCALE, 2160 * Vision.SCALE);
 
 				current_screen.render();
-				current_screen.update(main_scene);
-	    	}
+				current_screen.update(scene);
+			}
 		});
 		renderCycle.getKeyFrames().add(kf);
 		renderCycle.play();
-		
+		/*
+		 * Render loop (0.016 = 1/60 = 60FPS)
+		 */
+
 		main_stage.show();
 	}
-	
+
+	/**
+	 * Sets all possible icons for the program
+	 */
+	private void setIcons(Stage stage) {
+		stage.getIcons().add(new Image("file:assets/icons/1024.png"));
+		stage.getIcons().add(new Image("file:assets/icons/512.png"));
+		stage.getIcons().add(new Image("file:assets/icons/256.png"));
+		stage.getIcons().add(new Image("file:assets/icons/128.png"));
+		stage.getIcons().add(new Image("file:assets/icons/64.png"));
+		stage.getIcons().add(new Image("file:assets/icons/48.png"));
+		stage.getIcons().add(new Image("file:assets/icons/32.png"));
+		stage.getIcons().add(new Image("file:assets/icons/24.png"));
+		stage.getIcons().add(new Image("file:assets/icons/16.png"));
+	}
+
 	/**
 	 * Shorthand method for setting the currently displayed screen
 	 */
@@ -191,11 +153,11 @@ public class Vision extends Application {
 		current_screen = screen;
 		current_screen.start();
 	}
-	
-	public static void setScreen(Screen screen, Listener listener) {
-		server.removeListener(main_listener);
-		server.addListener(listener);
-		current_screen = screen;
-		current_screen.start();
+
+	/**
+	 * Checks whether or not an internet connection is detected
+	 */
+	public static void isNetwork() {
+
 	}
 }
